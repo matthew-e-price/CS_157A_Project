71a415d061ecbc7a9cadf8cb220d7f32771e56a7
price <priceypersonal@hotmail.com>
2023-11-26 03:40:15 -0800

    Origin

---
+++ cs157_group_project/src/main/java/com/cs157_group_project/Cs157GroupProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cs157_group_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs157GroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs157GroupProjectApplication.class, args);
+	}
+
+}
---
+++ cs157_group_project/src/test/java/com/cs157_group_project/Cs157GroupProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs157_group_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs157GroupProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
